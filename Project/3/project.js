18f1cea3c831d4e6f6281debbf4ab2efe51dfc45
Mai Đức Huy <huy.mai.set18@eiu.edu.vn>
2022-03-25 16:22:55 +0700

    23/3
    project

--- Project/3/project.js
+++ Project/3/project.js
@@ -1,3 +1,4 @@
+ /*GoTop Function*/
  $(document).ready(function() {
 	$(window).scroll(function() {
  		if ($(this).scrollTop() > 100) {
@@ -7,7 +8,7 @@
  		}
  	});
 
- 	$("GoTop").click(function(){
+ 	$("#GoTop").click(function(){
  		$("html, body").animate({
  				scrollTop: 0
  			},
@@ -15,3 +16,17 @@
  		);
  	});
  });
+/*Hover show menu*/
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
